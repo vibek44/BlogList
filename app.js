11aef49dd487a719f618f91399b60c40ef5e085b
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-21 16:05:23 +0200

    correcting lint error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose=require('mongoose')
 
 
 mongoose.connect(config.MONGODB_URI)
-  .then(()=> console.log('connected'))
+  .then(() => console.log('connected'))
   .catch(() => console.log('error connection'))
 
 app.use(cors())
