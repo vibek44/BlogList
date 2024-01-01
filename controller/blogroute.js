d79cb0d8ef595847b898dcda436399e44f64e1da
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-01 17:28:08 +0200

    Adding backend deletion functionality

--- controller/blogroute.js
+++ controller/blogroute.js
@@ -1,17 +1,29 @@
 const blogrouter=require('express').Router()
 const Blog=require('../models/bloglist')
 
-blogrouter.get('/blogs',(req,res) => {
-  Blog.find({})
-    .then((result) => res.json(result) )
-  
+blogrouter.get('/api/blogs',async(req,res) => {
+  const result = await Blog.find({})
+  res.json(result)   
 })
 
-blogrouter.post('/blogs',(req,res) => {
-  const blog=new Blog(req.body)
-  blog.save()
-    .then((result) => res.status(201).json(result))
-  
+blogrouter.post('/api/blogs',async(req,res) => {
+  let blog=req.body
+  if(!(blog.url && blog.title )){
+    return res.status(400).end()
+  }
+  if(!blog.likes) {
+    const newBlog =new Blog({ ...blog,likes:0 })
+    const result=await newBlog.save()
+    return res.status(201).send(result) 
+  }
+  blog=new Blog(blog)
+  const result=await blog.save()
+  res.status(201).json(result) 
+})
+
+blogrouter.delete('/api/blogs/:id', async(req,res) => {
+  await Blog.findByIdAndDelete(req.params.id)
+  return res.status(204).end()
   
 })
 
