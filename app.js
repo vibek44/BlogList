b9093b6bac0a285237cdb17588b0666d4e9d8ac3
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-08 19:08:16 +0200

    route added for login and users

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const config=require('./utils/config')
-const cors=require('cors')
 const express=require('express')
-const blogrouter=require('./controller/blogroute')
+require('express-async-errors')
 const app=express()
+const cors=require('cors')
+const middleware=require('./middleware')
+const loginRouter=require('./controller/login')
+const userRouter=require('./controller/usersroute')
+const blogRouter=require('./controller/blogroute')
+
 const mongoose=require('mongoose')
 
 
@@ -12,10 +17,11 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.use('/api',blogrouter)
-
-
-
+app.use(middleware.tokenExtractor)
+//app.use('/api/blogs',middleware.userExtractor,blogRouter)
+app.use('/api/blogs',blogRouter)
+app.use('/api/users',userRouter)
+app.use('/api/login',loginRouter)
 
 module.exports=app
+
