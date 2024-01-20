5c7aced3427d15653fa2daf56f35ff7f2d162ea1
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-20 19:16:52 +0200

    adding unknownend point and error-handling middleware function

--- middleware.js
+++ middleware.js
@@ -15,5 +15,25 @@ const userExtractor= async(req,res,next) => {
   next()
 }
 
-module.exports={ tokenExtractor,userExtractor }
+const unknownEndPoint=(req,res) => {
+  return res.status(404).send({ error:'unknown endpoint' })
+}
+
+const errorHandler=(error,req,res,next) => {
+  if(error.name==='CastError'){
+    return res.status(400).send({ error:'malformatted id:' })
+  }else if(error.name==='ValidationError'){
+    return res.status(400).json({ error:error.message })
+  }else if(error.name==='JsonWebTokenError'){
+    return res.status(401).json({ error:'invalid token' })
+  }
+  else if(error.name==='TokenExpiredError'){
+    return res.status(401).json({ error:'token expired' })
+  }
+  next(error)
+}
+
+
+
+module.exports={ tokenExtractor,userExtractor,unknownEndPoint,errorHandler }
 
