d6c40db0cba4b34f0f589033bfc19672ae724c3a
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-03-21 17:08:10 +0200

    adding schemapath comments to blog schema and using array schemaType for path

--- models/bloglist.js
+++ models/bloglist.js
@@ -1,23 +1,26 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const blogSchema=new mongoose.Schema({
-  title:String,
-  author:String,
-  url:String,
-  likes:Number,
-  user:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'User'
-  }
-})
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+});
 
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-blogSchema.set('toJSON' ,{
-  transform:(document, returnedObject) => {
-    returnedObject.id=returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-module.exports=mongoose.model('Blog',blogSchema)
+module.exports = mongoose.model("Blog", blogSchema);
